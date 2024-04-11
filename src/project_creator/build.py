07917d3b9946f1fb8e2afbd9550851150b75b743
= <leomi12042004@gmail.com>
2024-04-11 22:38:03 +0200

    directory help, exists safeguard

--- src/project_creator/build.py
+++ src/project_creator/build.py
@@ -1,18 +1,19 @@
 """Create the file and folder structure outlined by this project."""
 
-from pathlib import Path
-import re
 import os
+import re
+from pathlib import Path
 
 _help_dict = {
-    "name": 'This will be the name of the main folder in your projects "src"-directory.\n'
+    "name": 'This will be the name of the main folder in your projects "src" directory.\n'
     'Only alphanumeric characters and "_" are permitted. All other characters are escaped with "_".\n\n'
     'Example: Look at the structure of "Project Creator". There is a folder called "src" and inside\n'
     'is a folder called "project_creator". This is what the name refers to.\n'
-    'For a more detailed example refer to the section "[Naming Convention]" in README.md',
-    # TODO implement help for directory
-    "directory": ""
-    'For a more detailed example refer to the section "[Naming Convention]" in README.md',
+    'For a more detailed example refer to the section "[Naming Convention]" in "README.md"',
+    "directory": "This will be the parent folder for your new project. Upon project initialization\n"
+    "a new folder is created here using the naming convention for [home dir] and every file is put\n"
+    "into there.\n"
+    'For a more detailed example refer to the section "[Naming Convention]" in "README.md"',
 }
 
 
@@ -31,7 +32,7 @@ def main():
         "+-------------------------------+\n"
         "\n"
         'To get help about the current step, type "?help".\n'
-        'To quit without creating a project, type "?quit".'
+        'To quit without creating a project, type "?quit".\n'
     )
 
     while True:
@@ -53,10 +54,10 @@ def main():
 
     while True:
         # continuously ask for directory until a valid one is provided
-        proj_dir = Path(input("Enter your projects home directory: "))
-        str_proj_dir = str(proj_dir)
+        parent_dir = Path(input("Enter your projects home directory: "))
+        str_parent_dir = str(parent_dir)
 
-        match str_proj_dir.lower():
+        match str_parent_dir.lower():
             case "?help":
                 get_help("directory")
                 continue
@@ -69,13 +70,32 @@ def main():
                 # double backslash to prevent clash in regex
                 sep = "\\\\" if os.sep == "\\" else os.sep
                 pattern = rf"[^\w_. -{sep}]"
-                proj_dir = Path(re.sub(pattern, "_", str_proj_dir))
+                parent_dir = Path(re.sub(pattern, "_", str_parent_dir))
                 break
 
-    proj_dir.mkdir(exist_ok=True)
+    proj_folder = " ".join(word.capitalize() for word in proj_name.split("_"))
+    print(proj_folder)
 
-    print(f"{proj_name = }")
-    print(" ".join(word.capitalize() for word in proj_name.split("_")))
+    proj_dir = parent_dir / proj_folder
+
+    try:
+        proj_dir.mkdir()
+    except FileExistsError:
+        replace_existing = input(
+            "The selected project already exists. Continuing may override important\n"
+            "data. Do you want to continue anyway? (y/n): "
+        )
+
+        match replace_existing.lower():
+            case "y" | "yes":
+                pass
+
+            case "n" | "no" | _:
+                exit(1)
+        
+        del replace_existing
+    
+    
 
 
 if __name__ == "__main__":
